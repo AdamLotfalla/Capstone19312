aba121cc5c4111c5230aa4fb300d0b81688c8381
AdamLotfalla <144452542+AdamLotfalla@users.noreply.github.com>
2024-09-28 13:54:56 +0300

    added blink funcitonality every 5sec

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,16 @@
 
 #define IDK 3
 
+bool ON = true;
+
 void setup(){
   pinMode(IDK, INPUT);
+  digitalWrite(IDK, LOW);
 }
 
 void loop(){
-
+  digitalWrite(IDK, ON);
+  ON = !ON;
+  delay(5000);
+  // adjust delay if needed
 }
