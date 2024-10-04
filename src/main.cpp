e15a7c69cbe14bfc8a9a673985e6263a27b0e11d
AdamLotfalla <144452542+AdamLotfalla@users.noreply.github.com>
2024-10-04 15:12:46 +0300

    Added a "Hello world!" display for the e-paper

--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,45 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
-// #include <Ethernet.h>
-// #include <EthernetClient.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 
-#define WIFI_SSID "Redmi Note 10S"
-#define WIFI_PASSWORD "11111111"
-//OPPO Reno8 T 5G, 12348765
+#include "GxEPD2_display_selection_new_style.h"
 
-#define led 23
-//https://docs.google.com/spreadsheets/d/1S6_IL7yn1GuPK6xeMa3itCuH75X0g4UemVOapFCJXmc/edit?gid=0#gid=0
-//1S6_IL7yn1GuPK6xeMa3itCuH75X0g4UemVOapFCJXmc
+// alternately you can copy the constructor from GxEPD2_display_selection.h or GxEPD2_display_selection_added.h to here
+// e.g. for Wemos D1 mini:
+//GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4)); // GDEH0154D67
 
-String URL = "https://script.google.com/macros/s/AKfycbyXmQYj_BsG6RlaBTjZySvpfMkLCk3HEpxNikECkL3xlvlD_A282rAniDI4T9mdBlRw/exec?sts=write";
+// for handling alternative SPI pins (ESP32, RP2040) see example GxEPD2_Example.ino
 
+const char HelloWorld[] = "Hello World!";
 
-
-void setup(){
-
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-
-  Serial.begin(74880);
-  Serial.println();
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(300);
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
-
+  while (display.nextPage());
 }
 
-int counter = 0;
-
-void loop(){
-  if(WiFi.status() == WL_CONNECTED){
-    digitalWrite(led, HIGH);
-  }
-  else{
-    digitalWrite(led, LOW);
-  }
-  counter ++;
-  delay(3000);
-  if(counter == 15){
-    counter = 0;
-    URL += "&temp=" + String(69);
-    URL += "&humd=" + String(150);
-    URL += "&npeople=" + String(1000);
-
-
-    //::::::::::::::::::The process of sending or writing data to Google Sheets.
-    // Initialize HTTPClient as "http".
-    HTTPClient http;
-
-    // HTTP GET Request.
-    http.begin(URL.c_str());
-    http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-
-    // Gets the HTTP status code.
-    int httpCode = http.GET(); 
-    Serial.print("HTTP Status Code : ");
-    Serial.println(httpCode);
-
-    Serial.println("Sending data to Google spreadsheet...");
-    Serial.print("URL: ");
-    Serial.print(URL);
-    Serial.println();
-
-    // Getting response from google sheets.
-    String payload;
-    if (httpCode > 0) {
-      payload = http.getString();
-      Serial.println("Payload : " + payload);    
-    }
-    
-    http.end();
-    //::::::::::::::::::
-  }
+void setup()
+{
+  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
+  display.init(115200, true, 2, false); // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  helloWorld();
+  display.hibernate();
 }
+
+
+
+void loop() {};
